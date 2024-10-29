94cebfc548d4dede30779517aa3b694cd4cec641
speier-v <127931979+speier-v@users.noreply.github.com>
2024-10-29 23:10:16 +0100

    Implement action and spells component

--- frontend/src/app/modules/page/character-sheet/presentation/character-sheet.component.ts
+++ frontend/src/app/modules/page/character-sheet/presentation/character-sheet.component.ts
@@ -3,11 +3,12 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { StatDisplayComponent } from './stat-display/stat-display.component';
 import { SavingThrowsDisplayComponent } from './saving-throws-display/saving-throws-display.component';
 import { SkillsDisplayComponent } from './skills-display/skills-display.component';
+import { AbilitiesAndSpellsComponent } from './abilities-and-spells/abilities-and-spells.component';
 
 @Component({
   selector: 'character-sheet',
   standalone: true,
-  imports: [TopBarComponent, StatDisplayComponent, SavingThrowsDisplayComponent, SkillsDisplayComponent],
+  imports: [TopBarComponent, StatDisplayComponent, SavingThrowsDisplayComponent, SkillsDisplayComponent, AbilitiesAndSpellsComponent],
   templateUrl: './character-sheet.component.html',
   styleUrl: './character-sheet.component.css'
 })
