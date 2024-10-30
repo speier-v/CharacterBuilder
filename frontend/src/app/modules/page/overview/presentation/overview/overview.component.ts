0d65ae9627305926649232fba193eb324a18173c
pw380x <pw380x@gmail.com>
2024-10-30 08:22:29 +0100

    fix(character overview): add routing to "create character button"

--- frontend/src/app/modules/page/overview/presentation/overview/overview.component.ts
+++ frontend/src/app/modules/page/overview/presentation/overview/overview.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {CharacterCardComponent} from '../character-card/character-card.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'character-cards-overview',
   standalone: true,
   imports: [
-    CharacterCardComponent
+    CharacterCardComponent,
+    RouterLink
   ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
