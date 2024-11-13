112b1a078189229222bb72b05b39a78130e661e5
pw380x <pw380x@gmail.com>
2024-11-13 12:31:49 +0100

    feat(backend): added first test for connection from frontend to backend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,33 +1,53 @@
-import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient} from '@angular/common/http';
 import keycloakConfig from './keycloak.config';
-import { KeycloakService } from 'keycloak-angular';
+import {KeycloakService} from 'keycloak-angular';
+import {ApiService} from "./modules/core/api/api.service";
+import {environment} from "../environments/environment";
 
 function initializeKeycloak(keycloak: KeycloakService) {
-  return () =>
-    keycloak.init({
-      config: keycloakConfig.config,
-      initOptions: keycloakConfig.initOptions,
-    });
+    return () =>
+        keycloak.init({
+            config: keycloakConfig.config,
+            initOptions: keycloakConfig.initOptions,
+        });
+}
+
+function testApiConnection(apiService: ApiService) {
+    return () => apiService.get(`${environment.backendUrl}/test`).subscribe({
+            next: value => {
+                console.log(value);
+            }, error: err => {
+                console.error(err);
+            }
+        }
+    );
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    provideHttpClient(),
-    KeycloakService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
-      deps: [KeycloakService],
-      multi: true,
-    },
-  ],
+    providers: [
+        provideZoneChangeDetection({eventCoalescing: true}),
+        provideRouter(routes),
+        provideAnimationsAsync(),
+        provideHttpClient(),
+        KeycloakService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeKeycloak,
+            deps: [KeycloakService],
+            multi: true,
+        },
+        ApiService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: testApiConnection,
+            deps: [ApiService],
+            multi: true,
+        },
+    ],
 };
 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   keycloakUrl: 'http://localhost:9090',
   keycloakRealmName: 'charactyr-users',
   keycloakClientId: 'charactyr-frontend',
+  backendUrl: 'http://localhost:8090/api',
 };
