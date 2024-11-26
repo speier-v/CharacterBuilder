8f6ddc3cb1702f0a221b48029c4109830a5d926a
pw380x <pw380x@gmail.com>
2024-11-26 21:44:55 +0100

    !refactor(auth): removed login and sign up pages in favor of keycloak pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,21 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './modules/page/overview/presentation/overview/overview.component';
 import { CharacterEditorComponent } from './modules/page/character-editor/presentation/character-editor.component';
 import { CharacterSheetComponent } from './modules/page/character-sheet/presentation/character-sheet.component';
-import {
-  RequestPasswordResetComponent,
-} from './modules/page/login-and-sign-up/presentation/request-password-reset/request-password-reset.component';
-import {
-  PasswordResetComponent,
-} from './modules/page/login-and-sign-up/presentation/password-reset/password-reset.component';
-import { SignUpComponent } from './modules/page/login-and-sign-up/presentation/sign-up/sign-up.component';
-import { LoginComponent } from './modules/page/login-and-sign-up/presentation/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent, title: 'Charactyr Login' },
-  { path: 'login', component: LoginComponent, title: 'Charactyr Login' },
-  { path: 'sign-up', component: SignUpComponent, title: 'Sign up at Charactyr' },
-  { path: 'request-password-reset', component: RequestPasswordResetComponent, title: 'Request password reset' },
-  { path: 'reset-password', component: PasswordResetComponent, title: 'Reset your password' },
+  { path: '', redirectTo: 'overview', pathMatch: 'full' },
   { path: 'overview', component: OverviewComponent, title: 'Saved characters' },
   { path: 'character-editor', component: CharacterEditorComponent },
   { path: 'character-sheet', component: CharacterSheetComponent },
