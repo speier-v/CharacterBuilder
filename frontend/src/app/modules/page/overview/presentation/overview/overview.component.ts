67a6b4ce79e588d0eecf694e206849f1eb037a3e
pw380x <pw380x@gmail.com>
2024-11-30 23:23:52 +0100

    refactor(ux): replaced split button in overview with icon buttons

--- frontend/src/app/modules/page/overview/presentation/overview/overview.component.ts
+++ frontend/src/app/modules/page/overview/presentation/overview/overview.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { CharacterCardComponent } from '../character-card/character-card.component';
 import { CharacterGenService } from '../../../../character-model/character-gen.service';
 import { Character } from '../../../../character-model/character.model';
-import { RouterLink, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'character-cards-overview',
   standalone: true,
   imports: [
     CharacterCardComponent,
-    RouterLink,
-    CommonModule
+    CommonModule,
+    NgOptimizedImage,
   ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css',
@@ -22,7 +22,7 @@ export class OverviewComponent {
 
   constructor(
     private characterService: CharacterGenService,
-    private router: Router
+    private router: Router,
   ) {
     this.characters = characterService.getCharacters();
   }
